940fd9091da4f34611c5896cda3a0df9a8c01636
Alcione <alcionefribeiro@gmail.com>
2022-02-20 21:11:35 +0100

    Learning callback functions

--- TS-Course_Udemy/Second-Project/app.js
+++ TS-Course_Udemy/Second-Project/app.js
@@ -5,5 +5,16 @@ function printResult(num) {
     console.log('Result: ' + num);
 }
 printResult(add(12, 67));
-var combineValues = add;
+// CallBack function
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+addAndHandle(10, 20, function (result) {
+    console.log('addAndHandle: ' + result);
+});
+// Function types allows us to describe which type of functions I want use
+var combineValues;
+combineValues = add;
+// combineValues = printResult;
 console.log(combineValues(8, 8));
