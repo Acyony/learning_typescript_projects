4c1fd40dbcd03669e55d13a948950e94e99574bd
Alcione <alcionefribeiro@gmail.com>
2022-02-22 19:22:41 +0100

    setting compilation targets

---
+++ TS-Course_Udemy/Undertanding-TS/app.js
@@ -0,0 +1,11 @@
+"use strict";
+const userName = 'Alcione';
+let age;
+age = 30;
+var userInput;
+userInput = 'data';
+console.log(userName);
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    console.log('Clicked!');
+});
