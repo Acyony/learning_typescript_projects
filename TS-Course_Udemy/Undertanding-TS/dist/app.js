056bcce769ad6595c066f97041dcf66ebc46cc77
Alcione <alcionefribeiro@gmail.com>
2022-02-24 08:39:47 +0100

    rest operator

--- TS-Course_Udemy/Undertanding-TS/dist/app.js
+++ TS-Course_Udemy/Undertanding-TS/dist/app.js
@@ -17,4 +17,20 @@ const printOutput = output => console.log(output);
 const buttonEvent = document.querySelector('button');
 buttonEvent.addEventListener('click', event => console.log(event));
 console.log(add(9, 10));
+const hobbies = ['reading', 'crocheting', 'traveling'];
+const activeHobbies = ['fotography', ...hobbies];
+activeHobbies.push(...hobbies);
+const person = {
+    name: 'Alcione',
+    age: 33
+};
+const copiedPerson = Object.assign({}, person);
+console.log(copiedPerson);
+const adding = (...numbers) => {
+    return numbers.reduce((curResult, curValue) => {
+        return curResult + curValue;
+    }, 0);
+};
+const addNumbers = adding(5, 15, 56, 25);
+console.log('addNumbers: ', addNumbers);
 //# sourceMappingURL=app.js.map
